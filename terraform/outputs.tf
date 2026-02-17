d80a2fece148124166d7e808761bbee1b15d8d94
Ahs23 <armaanhshaikh@gmail.com>
2026-02-17 13:57:00 +0530

    Add SSH connection output for Ansible integration

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,10 @@
-output "instance public ip" {
+output "instance_public ip" {
   description = "Public IP of the EC2 instance"
   value = aws_instance.devops_server.public_ip
+}
+
+output "ssh_connection_command" {
+  description = "SSH command to connect to EC2 instance"
+  value = "ssh ec2-user@${aws_instance.devops_server.public_ip}"  
+  
 }
