45cc1443189de8b47a02e8e1a2c5748286e994f9
Ahs23 <armaanhshaikh@gmail.com>
2026-02-17 13:36:25 +0530

    Add Terraform output for EC2 public IP

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,52 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+resource "aws_security_group" "devops_sg" {
+  name        = "devops-project-sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "devops_server" {
-  ami           = "ami-0f5ee92e2d63afc18" # Amazon Linux 2 (example)
+  ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.devops_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install docker -y
+              systemctl start docker
+              systemctl enable docker
+              EOF
 
   tags = {
     Name = "devops-project-server"
   }
+}
+
+output "instance public ip" {
+  description = "Public IP of the EC2 instance"
+  value = aws_instance.devops_server.public_ip
 }
